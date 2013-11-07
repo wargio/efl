b5b7c0976e5a1b2b9982dd645678c6c0ca7e12c2
Daniel Zaoui <daniel.zaoui@samsung.com>
2013-11-08 00:09:14 +0200

    Remove useless code

--- src/lib/edje/edje_pick_core.c
+++ src/lib/edje/edje_pick_core.c
@@ -1,8 +1,96 @@
-#include "edje_pick_private.h"
 #include "Edje_Pick.h"
+#include "edje_private.h"
 
 static Edje_Pick *context = NULL;
 static unsigned char *init_count = 0;
+
+#define EDJE_PICK_HELP_STRING \
+"\nEdje Pick - the \"edj\" merging tool.\n\
+===================================\n\n\
+Use Edje Pick to compose a single edj file \
+by selecting groups from edj files.\n\n\
+Use Edje Pick as follows:\n\
+Include files with \'--include\' file-name\n\
+Follow the included file-name by names of selected groups from this file.\n\
+To select a group use: \'--group\' group-name.\n\n\
+You must specify merged file name with \'--output\' file-name.\n\
+Use '--verbose' switch to get detailed log.\n\n\
+To produce 3rd file \'out.edj\' that composed  of:\n\
+Group \'test\' from \'ex1.edj\' and \'test2\', \'test3\', from \'ex2.edj\'\n\
+edje_pick -i ex1.edj -g test -i ex2.edj -g test2 -g test3 -o out.edj\n\n\
+By using \'--append\' whole file content is selected.\n\
+No need to specify selected groups with this switch.\n\
+Note that selected group with will override group with the same name\n\
+from appended-file when using \'--append\' switch.\n\n\
+Example, the command:\n\
+edje_pick -a theme1.edj -i default.edj -g elm/button/base/default \
+-g elm/check/base/default -o hybrid.edj\n\n\
+will produce a 3rd theme file \'hybrid.edj\',\n\
+composed of all theme1.edj components.\n\
+Replacing the button and check with widgets taken from default theme.\n\
+(Given that theme1.edj button, check group-name are as in default.edj)\n"
+
+#define VERBOSE(COMMAND) if (context->v) { COMMAND; }
+
+struct _Edje_Pick_Id
+{
+   int old_id;
+   int new_id;
+   Eina_Bool used;
+};
+typedef struct _Edje_Pick_Id Edje_Pick_Id;
+
+struct _Edje_Pick_File_Params
+{
+   const char *name;
+   Eina_List *groups;
+   Edje_File *edf;     /* Keeps all file data after reading  */
+   Eina_Bool append;   /* Take everything from this file */
+
+   /* We hold list of IDs for each file */
+   Eina_List *scriptlist;
+   Eina_List *luascriptlist;
+   Eina_List *imagelist;
+   Eina_List *imagesetlist;  /* List of IDs (Edje_Pick_Data) for image sets */
+   Eina_List *samplelist;
+   Eina_List *tonelist;
+};
+typedef struct _Edje_Pick_File_Params Edje_Pick_File_Params;
+
+struct _Edje_Pick_Data
+{
+   const char *filename;  /* Image, Sample File Name       */
+   void *entry ;          /* used to build output file dir FIXME: REMOVE THIS */
+   void *data;            /* Data as taken from input file */
+
+   int size;
+   Edje_Pick_Id id;
+};
+typedef struct _Edje_Pick_Data Edje_Pick_Data;
+
+struct _Edje_Pick_Tone
+{
+   Edje_Sound_Tone *tone;
+   Eina_Bool used;
+};
+typedef struct _Edje_Pick_Tone Edje_Pick_Tone;
+
+struct _Edje_Pick_Font
+{
+   Edje_Font *f;
+   Eina_Bool used;
+};
+typedef struct _Edje_Pick_Font Edje_Pick_Font;
+
+struct _Edje_Pick
+{
+   Eina_Bool v; /* Verbose */
+   int current_group_id;
+   Edje_Pick_File_Params *current_file;
+   Eina_List *fontlist;
+   const char *errfile;
+   const char *errgroup;
+};
 
 EAPI void
 edje_pick_init(void)
@@ -54,7 +142,7 @@ edje_pick_context_set(Edje_Pick *c)
    context = c;
 }
 
-EAPI void
+static void
 _edje_pick_data_free(Eina_List *l)
 {
    Edje_Pick_Data *ep;
@@ -67,7 +155,7 @@ _edje_pick_data_free(Eina_List *l)
      }
 }
 
-EAPI void
+static void
 _edje_pick_out_file_free(Edje_File *out_file)
 {
    if (out_file)
@@ -112,7 +200,7 @@ _edje_pick_out_file_free(Edje_File *out_file)
      }
 }
 
-EAPI void
+static void
 _edje_pick_args_show(Eina_List *ifs, char *out)
 {  /* Print command-line arguments after parsing phase */
    Edje_Pick_File_Params *p;
@@ -188,7 +276,7 @@ edje_pick_err_str_get(Edje_Pick_Status s)
    return err;
 }
 
-EAPI int
+static int
 _edje_pick_cleanup(Eina_List *ifs, Edje_File *out_file, Edje_Pick_Status s,
       Eina_List *a_files, Eina_List *i_files, Eina_List *groups,
       const char *errfile, const char *errgroup)
@@ -259,7 +347,7 @@ _edje_pick_cleanup(Eina_List *ifs, Edje_File *out_file, Edje_Pick_Status s,
 }
 
 /* Look for group name in all input files that are not d1 */
-EAPI int
+static int
 _group_name_in_other_file(Eina_List *inp_files, void *d1, void *d2)
 {
    Edje_Pick_File_Params *inp_file = d1;
@@ -462,7 +550,7 @@ edje_pick_command_line_parse(int argc, char **argv,
    return EDJE_PICK_NO_ERROR;
 }
 
-EAPI void
+static void
 _edje_pick_external_dir_update(Edje_File *o, Edje_File *edf)
 {
    if (edf->external_dir && edf->external_dir->entries_count)
@@ -489,7 +577,7 @@ _edje_pick_external_dir_update(Edje_File *o, Edje_File *edf)
      }
 }
 
-EAPI Edje_File *
+static Edje_File *
 _edje_pick_output_prepare(Edje_File *o, Edje_File *edf, char *name)
 {
    /* Allocate and prepare header memory buffer */
@@ -533,7 +621,7 @@ _edje_pick_output_prepare(Edje_File *o, Edje_File *edf, char *name)
    return o;
 }
 
-EAPI int
+static int
 _edje_pick_header_make(Edje_File *out_file , Edje_File *edf, Eina_List *ifs)
 {
    Edje_Part_Collection_Directory_Entry *ce;
@@ -615,14 +703,14 @@ _edje_pick_header_make(Edje_File *out_file , Edje_File *edf, Eina_List *ifs)
    return status;
 }
 
-EAPI int
+static int
 _id_cmp(const void *d1, const void *d2)
 {
    /* Find currect ID struct */
    return (((Edje_Pick_Data *) d1)->id.old_id - ((intptr_t) d2));
 }
 
-EAPI int
+static int
 _edje_pick_new_id_get(Eina_List *id_list, int id, Eina_Bool set_used)
 {
    if (id >= 0)
@@ -644,7 +732,7 @@ _edje_pick_new_id_get(Eina_List *id_list, int id, Eina_Bool set_used)
    return id;
 }
 
-EAPI int
+static int
 _edje_pick_images_add(Edje_File *edf, Edje_File *o)
 {
    char buf[1024];
@@ -757,7 +845,7 @@ _edje_pick_images_add(Edje_File *edf, Edje_File *o)
    return status;
 }
 
-EAPI int
+static int
 _edje_pick_sounds_add(Edje_File *edf)
 {
    char buf[1024];
@@ -822,7 +910,7 @@ _edje_pick_sounds_add(Edje_File *edf)
    return status;
 }
 
-EAPI int
+static int
 _font_cmp(const void *d1, const void *d2)
 {
    const Edje_Font *f1 = d1;
@@ -833,7 +921,7 @@ _font_cmp(const void *d1, const void *d2)
            strcmp(f1->file, f2->file));
 }
 
-EAPI int
+static int
 _Edje_Pick_Fonts_add(Edje_File *edf)
 {
    Eet_Data_Descriptor *_font_list_edd = NULL;
@@ -874,7 +962,7 @@ _Edje_Pick_Fonts_add(Edje_File *edf)
    return EDJE_PICK_NO_ERROR;
 }
 
-EAPI int
+static int
 _edje_pick_scripts_add(Edje_File *edf, int id, int new_id)
 {
    int size;
@@ -907,7 +995,7 @@ _edje_pick_scripts_add(Edje_File *edf, int id, int new_id)
    return EDJE_PICK_NO_ERROR;
 }
 
-EAPI int
+static int
 _edje_pick_lua_scripts_add(Edje_File *edf, int id, int new_id)
 {
    int size;
@@ -940,7 +1028,7 @@ _edje_pick_lua_scripts_add(Edje_File *edf, int id, int new_id)
    return EDJE_PICK_NO_ERROR;
 }
 
-EAPI void
+static void
 _edje_pick_styles_update(Edje_File *o, Edje_File *edf)
 {
    /* Color Class in Edje_File */
@@ -951,7 +1039,7 @@ _edje_pick_styles_update(Edje_File *o, Edje_File *edf)
      o->styles = eina_list_append(o->styles, stl);
 }
 
-EAPI void
+static void
 _edje_pick_color_class_update(Edje_File *o, Edje_File *edf)
 {
    /* Color Class in Edje_File */
@@ -962,7 +1050,7 @@ _edje_pick_color_class_update(Edje_File *o, Edje_File *edf)
      o->color_classes = eina_list_append(o->color_classes, cc);
 }
 
-EAPI void
+static void
 _edje_pick_images_desc_update(Edje_Part_Description_Image *desc)
 {
    /* Update all IDs of images in descs */
@@ -994,7 +1082,7 @@ _edje_pick_images_desc_update(Edje_Part_Description_Image *desc)
      }
 }
 
-EAPI void
+static void
 _edje_pick_images_process(Edje_Part_Collection *edc)
 {
    /* Find what images are used, update IDs, mark as USED */
@@ -1018,7 +1106,7 @@ _edje_pick_images_process(Edje_Part_Collection *edc)
      }
 }
 
-EAPI int
+static int
 _sample_cmp(const void *d1, const void *d2)
 {
    /* Locate sample by name */
@@ -1032,7 +1120,7 @@ _sample_cmp(const void *d1, const void *d2)
    return 1;
 }
 
-EAPI int
+static int
 _tone_cmp(const void *d1, const void *d2)
 {
    /* Locate tone by name */
@@ -1046,7 +1134,7 @@ _tone_cmp(const void *d1, const void *d2)
    return 1;
 }
 
-EAPI void
+static void
 _edje_pick_program_update(Edje_Program *prog)
 {
    Edje_Pick_Data *p;
@@ -1071,7 +1159,7 @@ _edje_pick_program_update(Edje_Program *prog)
      t->used = EINA_TRUE;
 }
 
-EAPI int
+static int
 _edje_pick_programs_process(Edje_Part_Collection *edc)
 {
    /* This wil mark which samples are used and should be saved */
@@ -1095,7 +1183,7 @@ _edje_pick_programs_process(Edje_Part_Collection *edc)
    return EDJE_PICK_NO_ERROR;
 }
 
-EAPI int
+static int
 _edje_pick_collection_process(Edje_Part_Collection *edc)
 {
    /* Update all IDs, NAMES in current collection */
@@ -1109,7 +1197,7 @@ _edje_pick_collection_process(Edje_Part_Collection *edc)
    return EDJE_PICK_NO_ERROR;
 }
 
-EAPI void
+static void
 _edje_pick_sound_dir_compose(Eina_List *samples, Eina_List *tones, Edje_File *o)
 {  /* Compose sound_dir array from all used samples, tones */
    if (samples)
@@ -1552,12 +1640,14 @@ edje_pick_process(int argc, char **argv)
          NULL, NULL, NULL, NULL, NULL);
 }
 
-static Eina_Bool _play_finished(void *data (void), Eo *in, const Eo_Event_Description *desc EINA_UNUSED, void *event_info EINA_UNUSED)
+typedef void (*_finish_cb)();
+
+static Eina_Bool _play_finished(void *data, Eo *in, const Eo_Event_Description *desc EINA_UNUSED, void *event_info EINA_UNUSED)
 {
    printf("<%s> <%d>\n", __func__, __LINE__);
    eo_del(in);
-
-   data();
+   _finish_cb finish = data;
+   finish();
 
    return EINA_TRUE;
 }
